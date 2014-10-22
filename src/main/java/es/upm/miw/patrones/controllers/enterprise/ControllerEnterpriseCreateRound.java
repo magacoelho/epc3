8f12dff44cc720e238638c3b8566f076081500a3
magacoelho <mgacoelho@yahoo.es>
2014-10-23 00:51:27 +0200

    caso uso Create Round

---
+++ src/main/java/es/upm/miw/patrones/controllers/enterprise/ControllerEnterpriseCreateRound.java
@@ -0,0 +1,7 @@
+package es.upm.miw.patrones.controllers.enterprise;
+
+import es.upm.miw.patrones.controllers.ControllerCreateRound;
+
+public class ControllerEnterpriseCreateRound implements ControllerCreateRound{
+
+}
