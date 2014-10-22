9ac083aaf78e432d7c59e4a25aa81809fc3e2493
magacoelho <mgacoelho@yahoo.es>
2014-10-23 01:02:50 +0200

    Refactor MoveCard

--- src/main/java/es/upm/miw/patrones/controller/ControllerMoveRound.java
+++
@@ -1,5 +0,0 @@
-package es.upm.miw.patrones.controller;
-
-public interface ControllerMoveRound {
-
-}
--- src/main/java/es/upm/miw/patrones/views/mobile/ViewMobileMoveCard.java
+++ src/main/java/es/upm/miw/patrones/views/mobile/ViewMobileMoveCard.java
@@ -1,5 +1,5 @@
 package es.upm.miw.patrones.views.mobile;
 
-public class ViewMobileMoveRound {
+public class ViewMobileMoveCard {
 
 }
