9d49ec4d90a381fe23b1b8bff073c01830cf0afc
magacoelho <mgacoelho@yahoo.es>
2014-10-23 01:18:23 +0200

    Caso de uso Get Statistics

---
+++ src/main/java/es/upm/miw/patrones/controller/proxy/web/ControllerProxyWebGetStatistics.java
@@ -0,0 +1,5 @@
+package es.upm.miw.patrones.controller.proxy.web;
+
+public class ControllerProxyWebGetStatistics {
+
+}
---
+++ src/main/java/es/upm/miw/patrones/controllers/enterprise/ControllerEnterpriseGetStatistics.java
@@ -0,0 +1,7 @@
+package es.upm.miw.patrones.controllers.enterprise;
+
+import es.upm.miw.patrones.controllers.ControllerGetStatistics;
+
+public class ControllerEnterpriseGetStatistics implements ControllerGetStatistics{
+
+}
