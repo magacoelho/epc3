6ba2c49de134fd4be9ea581451ee8a366890c527
magacoelho <mgacoelho@yahoo.es>
2014-10-23 02:05:37 +0200

    Round DAO extends GenericaDAO

--- src/main/java/es/upm/miw/patrones/models/DAO/RoundDAO.java
+++ src/main/java/es/upm/miw/patrones/models/DAO/RoundDAO.java
@@ -1,5 +1,7 @@
 package es.upm.miw.patrones.models.DAO;
 
-public interface RoundDAO {
+import es.upm.miw.patrones.models.entities.Round;
+
+public interface RoundDAO extends GenericDAO<Round,Integer>{
 
 }
